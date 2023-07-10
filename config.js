702ff16b984f9a26d9f246da8dc59eb89e1568d5
Matthew Smith <mattysmithuk@googlemail.com>
2023-07-10 14:22:09 +0100

    add mongo config

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
+let mongodb = process.env.MONGODB_URI || 'mongodb://localhost:27017/radiusdb';
 module.exports = {
-    'database': 'mongodb://localhost:27017/radiusdb'
+    'database': mongodb,
 };
